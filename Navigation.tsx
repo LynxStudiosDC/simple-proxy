ebe181795ec6996e53db7c89513111a6629ddc73
LynxStudios <108672750+LynxStudiosDC@users.noreply.github.com>
2024-01-09 21:22:57 -0600

    Update Navigation.tsx

--- Navigation.tsx
+++ Navigation.tsx
@@ -86,7 +86,20 @@ export function Navigation(props: NavigationProps) {
               >
                 <BrandPill clickable />
               </Link>
-              
+               <a
+                href={conf().DISCORD_LINK}
+                target="_blank"
+                rel="noreferrer"
+                className="text-xl text-white tabbable rounded-full"
+              >
+                <IconPatch icon={Icons.DISCORD} clickable downsized />
+              </a>
+              <a
+                href={conf().GITHUB_LINK}
+                target="_blank"
+                rel="noreferrer"
+                className="text-xl text-white tabbable rounded-full"
+              >
             </div>
             <div className="relative pointer-events-auto">
               <LinksDropdown>
